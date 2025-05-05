d80d3ff67f5752e1bf67f10dc9a19554d67b363c
yuluyuluyulu <3341904198@qq.com>
2025-05-06 02:54:35 +0800

    sage 3.0
    3.0

--- sage/build/Desktop_Qt_6_5_3_MinGW_64_bit-Debug/debug/moc_mainscene.cpp
+++ sage/build/Desktop_Qt_6_5_3_MinGW_64_bit-Debug/debug/moc_mainscene.cpp
@@ -38,20 +38,36 @@ namespace {
 #ifdef QT_MOC_HAS_STRINGDATA
 struct qt_meta_stringdata_CLASSMainSceneENDCLASS_t {};
 static constexpr auto qt_meta_stringdata_CLASSMainSceneENDCLASS = QtMocHelpers::stringData(
-    "MainScene"
+    "MainScene",
+    "gameOverSignal",
+    "",
+    "updateGameTime",
+    "gameOver"
 );
 #else  // !QT_MOC_HAS_STRING_DATA
 struct qt_meta_stringdata_CLASSMainSceneENDCLASS_t {
-    uint offsetsAndSizes[2];
+    uint offsetsAndSizes[10];
     char stringdata0[10];
+    char stringdata1[15];
+    char stringdata2[1];
+    char stringdata3[15];
+    char stringdata4[9];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(sizeof(qt_meta_stringdata_CLASSMainSceneENDCLASS_t::offsetsAndSizes) + ofs), len 
 Q_CONSTINIT static const qt_meta_stringdata_CLASSMainSceneENDCLASS_t qt_meta_stringdata_CLASSMainSceneENDCLASS = {
     {
-        QT_MOC_LITERAL(0, 9)   // "MainScene"
+        QT_MOC_LITERAL(0, 9),  // "MainScene"
+        QT_MOC_LITERAL(10, 14),  // "gameOverSignal"
+        QT_MOC_LITERAL(25, 0),  // ""
+        QT_MOC_LITERAL(26, 14),  // "updateGameTime"
+        QT_MOC_LITERAL(41, 8)   // "gameOver"
     },
-    "MainScene"
+    "MainScene",
+    "gameOverSignal",
+    "",
+    "updateGameTime",
+    "gameOver"
 };
 #undef QT_MOC_LITERAL
 #endif // !QT_MOC_HAS_STRING_DATA
@@ -63,12 +79,26 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainSceneENDCLASS[] = {
       11,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       0,       // signalCount
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   32,    2, 0x06,    1 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       3,    0,   33,    2, 0x08,    2 /* Private */,
+       4,    0,   34,    2, 0x08,    3 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
 
        0        // eod
 };
@@ -81,16 +111,38 @@ Q_CONSTINIT const QMetaObject MainScene::staticMetaObject = { {
     nullptr,
     qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSMainSceneENDCLASS_t,
         // Q_OBJECT / Q_GADGET
-        QtPrivate::TypeAndForceComplete<MainScene, std::true_type>
+        QtPrivate::TypeAndForceComplete<MainScene, std::true_type>,
+        // method 'gameOverSignal'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'updateGameTime'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'gameOver'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
     >,
     nullptr
 } };
 
 void MainScene::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    (void)_o;
-    (void)_id;
-    (void)_c;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainScene *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->gameOverSignal(); break;
+        case 1: _t->updateGameTime(); break;
+        case 2: _t->gameOver(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (MainScene::*)();
+            if (_t _q_method = &MainScene::gameOverSignal; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 0;
+                return;
+            }
+        }
+    }
     (void)_a;
 }
 
@@ -110,6 +162,23 @@ void *MainScene::qt_metacast(const char *_clname)
 int MainScene::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 3)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 3;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 3)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 3;
+    }
     return _id;
+}
+
+// SIGNAL 0
+void MainScene::gameOverSignal()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
 }
 QT_WARNING_POP
--- sage/build/Desktop_Qt_6_5_3_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
+++ sage/build/Desktop_Qt_6_5_3_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -43,17 +43,19 @@ static constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers:
     "",
     "onMusicButtonClicked",
     "onVolumeSliderValueChanged",
-    "value"
+    "value",
+    "onGameOver"
 );
 #else  // !QT_MOC_HAS_STRING_DATA
 struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
-    uint offsetsAndSizes[12];
+    uint offsetsAndSizes[14];
     char stringdata0[11];
     char stringdata1[16];
     char stringdata2[1];
     char stringdata3[21];
     char stringdata4[27];
     char stringdata5[6];
+    char stringdata6[11];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(sizeof(qt_meta_stringdata_CLASSMainWindowENDCLASS_t::offsetsAndSizes) + ofs), len 
@@ -64,14 +66,16 @@ Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_st
         QT_MOC_LITERAL(27, 0),  // ""
         QT_MOC_LITERAL(28, 20),  // "onMusicButtonClicked"
         QT_MOC_LITERAL(49, 26),  // "onVolumeSliderValueChanged"
-        QT_MOC_LITERAL(76, 5)   // "value"
+        QT_MOC_LITERAL(76, 5),  // "value"
+        QT_MOC_LITERAL(82, 10)   // "onGameOver"
     },
     "MainWindow",
     "onButtonClicked",
     "",
     "onMusicButtonClicked",
     "onVolumeSliderValueChanged",
-    "value"
+    "value",
+    "onGameOver"
 };
 #undef QT_MOC_LITERAL
 #endif // !QT_MOC_HAS_STRING_DATA
@@ -83,7 +87,7 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
       11,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   14, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -91,14 +95,16 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   32,    2, 0x08,    1 /* Private */,
-       3,    0,   33,    2, 0x08,    2 /* Private */,
-       4,    1,   34,    2, 0x08,    3 /* Private */,
+       1,    0,   38,    2, 0x08,    1 /* Private */,
+       3,    0,   39,    2, 0x08,    2 /* Private */,
+       4,    1,   40,    2, 0x08,    3 /* Private */,
+       6,    0,   43,    2, 0x08,    5 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void,
 
        0        // eod
 };
@@ -118,7 +124,9 @@ Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'onVolumeSliderValueChanged'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        QtPrivate::TypeAndForceComplete<int, std::false_type>
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'onGameOver'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
     >,
     nullptr
 } };
@@ -132,6 +140,7 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 0: _t->onButtonClicked(); break;
         case 1: _t->onMusicButtonClicked(); break;
         case 2: _t->onVolumeSliderValueChanged((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 3: _t->onGameOver(); break;
         default: ;
         }
     }
@@ -156,13 +165,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 3)
+        if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
+        _id -= 4;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 3)
+        if (_id < 4)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 3;
+        _id -= 4;
     }
     return _id;
 }
